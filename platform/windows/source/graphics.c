afe2ba3e39ba7e8101f2691c3d1a90667b828fa6
thealing <159199097+thealing@users.noreply.github.com>
2026-04-05 02:42:42 +0200

    Fixed debug render

--- platform/windows/source/graphics.c
+++ platform/windows/source/graphics.c
@@ -395,6 +395,14 @@ void graphics_draw_circle(const Circle* circle, bool fill)
 	}
 
 	glEnd();
+
+	glBegin(GL_LINES);
+
+	glVertex2d(circle->center.x, circle->center.y);
+
+	glVertex2d(circle->center.x + circle->radius, circle->center.y);
+
+	glEnd();
 }
 
 void graphics_draw_polygon(const Polygon* polygon, bool fill)
